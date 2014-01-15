c386b74142a1d82550059e2a51a1bef558db8f93
qianjigui <qianjigui@gmail.com>
2014-01-15 11:54:34 +0800

    Add work support in environment

--- bin/work_config_git.sh
+++ bin/work_config_git.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-git config user.name pengcheng.wang
-git config user.email pengcheng.wang@alibaba-inc.com
+USERNAME=pengcheng.wang
+EMAIL=pengcheng.wang@alibaba-inc.com
+git config user.name $USERNAME
+git config user.email $EMAIL
+
+export GIT_AUTHOR_EMAIL=$EMAIL
+export GIT_COMMITTER_NAME=$USERNAME
+export GIT_COMMITTER_EMAIL=$EMAIL
