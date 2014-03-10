41049398b9703292ac3a7cedde0f4589d69c38a4
qianjigui <qianjigui@gmail.com>
2014-03-10 15:08:30 +0800

    Add comment for zsh

--- .zshcustomrc/pczsh.zsh
+++ .zshcustomrc/pczsh.zsh
@@ -3,6 +3,8 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+# curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#
 
 plugins=(ruby tmux git)
 setopt append_history no_inc_append_history no_share_history
