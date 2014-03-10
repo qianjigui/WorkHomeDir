1c27208f8ee8b8f55193b754da3bc10e01391f51
qianjigui <qianjigui@gmail.com>
2014-03-10 13:58:20 +0800

    Add zsh custom

---
+++ .zshcustomrc/pczsh.zsh
@@ -0,0 +1,8 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+plugins=(ruby tmux git)
+setopt append_history no_inc_append_history no_share_history
