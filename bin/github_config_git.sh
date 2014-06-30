e4d69d6610632ce215dc8cdff5469266029287b8
qianjigui <qianjigui@gmail.com>
2014-06-30 10:53:22 +0800

    Refactor gitignore

--- bin/github_config_git.sh
+++ bin/github_config_git.sh
@@ -5,6 +5,7 @@ EMAIL=qianjigui@gmail.com
 git config user.name $USERNAME
 git config user.email $EMAIL
 
+export GIT_AUTHOR_NAME=$USERNAME
 export GIT_AUTHOR_EMAIL=$EMAIL
 export GIT_COMMITTER_NAME=$USERNAME
 export GIT_COMMITTER_EMAIL=$EMAIL
