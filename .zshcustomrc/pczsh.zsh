ed13c0ec581ced4ce0d72b9b8401a5b6ec39bb3c
qianjigui <qianjigui@gmail.com>
2015-05-11 11:15:51 +0800

    Add android mac build tools

--- .zshcustomrc/pczsh.zsh
+++ .zshcustomrc/pczsh.zsh
@@ -33,9 +33,20 @@ if [ -d $MAC_GO_ROOT ]; then
 fi
 
 ##Android SDK
-SDK_DIR=$HOME/Library/Android/sdk/platform-tools/
-if [ -d $SDK_DIR ]; then
-    echo "Add Android SDK"
-    export PATH=$PATH:$SDK_DIR
+ANDROID_SDK_DIR=$HOME/Library/Android/sdk
+ANDROID_PLATFORM_TOOLS=$ANDROID_SDK_DIR/platform-tools
+ANDROID_BUILD_TOOLS=$ANDROID_SDK_DIR/build-tools
+if [ -d $ANDROID_PLATFORM_TOOLS ]; then
+    echo "Add Android SDK platform tools"
+    export PATH=$ANDROID_PLATFORM_TOOLS:$PATH
+fi
+
+if [ -d $ANDROID_BUILD_TOOLS ]; then
+    ANDROID_BUILD_TOOLS_LATEST_VERSION=$(ls $HOME/Library/Android/sdk/build-tools/ | sort | tail -1)
+    ANDROID_BUILD_TOOLS=$ANDROID_BUILD_TOOLS/$ANDROID_BUILD_TOOLS_LATEST_VERSION
+    if [ -d $ANDROID_BUILD_TOOLS ]; then
+        echo "Add Android SDK Build tools"
+        export PATH=$ANDROID_BUILD_TOOLS:$PATH
+    fi
 fi
 
