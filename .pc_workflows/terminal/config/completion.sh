fcaa9d01f9d27cd8202e24f3b5395aaca28a2d53
qianjigui <qianjigui@gmail.com>
2015-05-18 11:56:33 +0800

    Fix completion

--- .pc_workflows/terminal/config/completion.sh
+++ .pc_workflows/terminal/config/completion.sh
@@ -1,7 +1,8 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
-if [ -f /etc/bash_completion ] ; then
-    source /etc/bash_completion
+if [ -z $PCWF_IS_ZSH ]; then
+    if [ -f /etc/bash_completion ] ; then
+        source /etc/bash_completion
+    fi
 fi
-
