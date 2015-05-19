e2c2c81b777871875f1d211da67797882a598dec
qianjigui <qianjigui@gmail.com>
2015-05-19 12:15:31 +0800

    Add init

--- .pc_workflows/bootstraps.sh
+++ .pc_workflows/bootstraps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#source $HOME/.pc_workflows/bootstraps.sh
+
 #pc workflows dir
 export PCWF="$HOME/.pc_workflows"
 source $PCWF/terminal/rc.sh
--- .pc_workflows/init/bash.sh
+++ .pc_workflows/init/bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 BOOTSTRAPS="source $HOME/.pc_workflows/bootstraps.sh"
-echo $BOOTSTRAPS >> $HOME/.oh-my-zsh/custom/example.zsh
+echo $BOOTSTRAPS >> $HOME/.oh-my-zsh/custom/pcwf.zsh
 echo $BOOTSTRAPS >> $HOME/.bashrc
