175d6f874012db12d0246692ed48dfbac33afdf7
qianjigui <qianjigui@gmail.com>
2015-05-19 18:58:10 +0800

    Add vim complete meno close

--- .pc_workflows/terminal/cmd/find.sh
+++ .pc_workflows/terminal/cmd/find.sh
@@ -2,10 +2,17 @@
 
 pcwf_findsrc() {
     find $* -name .repo -prune -o -name .git -prune -o -type f \( \
-        -iname '*.c' -o -iname '*.cc' -o -iname '*.cpp' -o -iname '*.h' \
+        -iname '*.c' \
+        -o -iname '*.cc' \
+        -o -iname '*.cpp' \
+        -o -iname '*.h' \
+        -o -iname '*.hpp' \
+        -o -iname '*.hxx' \
         -o -iname '*.java' \
         -o -iname '*.xml' \
         -o -iname '*.mk' \
+        -o -iname '*.rb' \
+        -o -iname '*.go' \
         \) -print
 }
 
--- .pc_workflows/vim/plugin/YouCompleteMe.vim
+++ .pc_workflows/vim/plugin/YouCompleteMe.vim
@@ -1,2 +1,5 @@
 "https://github.com/Valloric/YouCompleteMe/issues/9
 let g:AutoClosePumvisible = {"ENTER": "<C-Y>", "ESC": "<ESC>"}
+"let g:ycm_autoclose_preview_window_after_completion = 1
+"let g:ycm_autoclose_preview_window_after_insertion = 1
+set completeopt=menu,menuone
