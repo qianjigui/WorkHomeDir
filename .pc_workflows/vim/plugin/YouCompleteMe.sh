e4bf477645204bfdce78691500a34798ab4457c9
qianjigui <qianjigui@gmail.com>
2015-05-20 10:23:16 +0800

    Add comment for YCM

--- .pc_workflows/vim/plugin/YouCompleteMe.sh
+++ .pc_workflows/vim/plugin/YouCompleteMe.sh
@@ -3,3 +3,6 @@
 cd $HOME/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer --gocode-completer  --system-libclang
 
+#download
+#http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR2/eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz
+echo "Please install elim: http://eclim.org/install.html"
