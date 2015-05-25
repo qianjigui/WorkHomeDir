ffc6c3d4dd9887c7a445cfb2f964e13cc7c370e2
qianjigui <qianjigui@gmail.com>
2015-05-25 20:13:10 +0800

    Add zsh install

--- .pc_workflows/init/bash.sh
+++ .pc_workflows/init/bash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#install curl, git
+#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 BOOTSTRAPS="source $HOME/.pc_workflows/bootstraps.sh"
 echo $BOOTSTRAPS >> $HOME/.oh-my-zsh/custom/pcwf.zsh
 echo $BOOTSTRAPS >> $HOME/.bashrc
