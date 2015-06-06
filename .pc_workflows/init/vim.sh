4a2cbe19483734b98336c206d381dec7ca38707c
qianjigui <qianjigui@gmail.com>
2015-06-06 12:00:53 +0800

    Add Vim env setup

--- .pc_workflows/init/vim.sh
+++ .pc_workflows/init/vim.sh
@@ -6,4 +6,8 @@ mkdir -p $HOME/.vim/bundle/
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 
-
+cat <<-END
+"For pc workflows
+let $PCWF="$HOME/.pc_workflows"
+source $PCWF/vim/rc.vim
+END > .vimrc
