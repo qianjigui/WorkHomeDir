51bca23247453cf4118c64ef277b6381fedcba86
qianjigui <qianjigui@gmail.com>
2015-07-23 10:02:42 +0800

    Add snip support

--- .pc_workflows/init/vim.sh
+++ .pc_workflows/init/vim.sh
@@ -6,8 +6,12 @@ mkdir -p $HOME/.vim/bundle/
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 
+#For UltiSnips
+ln -s $PCWF/vim/UltiSnips $HOME/.vim/UltiSnips
+
 cat <<-END
 "For pc workflows
 let $PCWF="$HOME/.pc_workflows"
 source $PCWF/vim/rc.vim
 END > .vimrc
+
