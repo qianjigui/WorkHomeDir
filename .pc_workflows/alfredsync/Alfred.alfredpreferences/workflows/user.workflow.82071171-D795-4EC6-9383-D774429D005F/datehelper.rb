39bdbdd6ad909971eea57f13f7537895edebdfea
qianjigui <qianjigui@gmail.com>
2015-09-12 12:41:16 +0800

    Format date

--- .pc_workflows/alfredsync/Alfred.alfredpreferences/workflows/user.workflow.82071171-D795-4EC6-9383-D774429D005F/datehelper.rb
+++ .pc_workflows/alfredsync/Alfred.alfredpreferences/workflows/user.workflow.82071171-D795-4EC6-9383-D774429D005F/datehelper.rb
@@ -10,6 +10,10 @@ require 'rubygems' unless defined? Gem
 require 'alfred'
 require 'date'
 
+def day_of_month(y,m)
+    Date.new(y,m,-1).day
+end
+
 Alfred.with_friendly_error do |alfred|
   alfred.with_rescue_feedback = true
 
@@ -17,8 +21,10 @@ Alfred.with_friendly_error do |alfred|
 
   t = Time.now
 
-  fb.add_item({title: t, subtitle: '%dth of week cost %f in this year' % [t.wday, t.yday/365.0]})
-  fb.add_item({title: '%dth day' % [t.yday], subtitle: '%dth week' % [(t.yday+6)/7]})
+  title = t.strftime('%F %T %z %A')
+  subtitle = '%s %.2fyear %.2fmonth' % [t.strftime('%jd/%Vw'), t.yday/365.0, t.mday*1.0/day_of_month(t.year, t.month)]
+
+  fb.add_item({title: title, subtitle: subtitle})
 
   puts fb.to_alfred
 end
