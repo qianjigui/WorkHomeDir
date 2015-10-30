44da7938e365cc2806eafa0b35f044d46efb08af
qianjigui <qianjigui@gmail.com>
2015-10-30 17:08:46 +0800

    Add dev environment

--- .pc_workflows/terminal/cmd/work.sh
+++ .pc_workflows/terminal/cmd/work.sh
@@ -1,7 +1,3 @@
-export A20='/SSHFS/pengcheng.wang/projs/tvs/Openbase/A20/2.xUserDebug'
-export A31='/SSHFS/pengcheng.wang/projs/tvs/Openbase/A31S4.4/YOS2/EngVersion'
-export L='/SSHFS/pengcheng.wang/projs/tvs/Openbase/rockchip/L'
-
 workgitconfig() {
     GIT_USERNAME=pengcheng.wang
     GIT_EMAIL=pengcheng.wang@alibaba-inc.com
--- .pc_workflows/terminal/config/host/WPCs-MacBook-Pro.local.sh
+++ .pc_workflows/terminal/config/host/WPCs-MacBook-Pro.local.sh
@@ -20,3 +20,11 @@ then
     export GOROOT=$_TMP
 fi
 
+#Env
+export A20='/SSHFS/pengcheng.wang/projs/tvs/Openbase/A20/2.xUserDebug'
+export A31='/SSHFS/pengcheng.wang/projs/tvs/Openbase/A31S4.4/YOS2/EngVersion'
+export L='/SSHFS/pengcheng.wang/projs/tvs/Openbase/rockchip/L'
+export SE="$HOME/workspace/work/seyunos"
+export AD="$HOME/workspace/work/adwords"
+export SelfDoc="$HOME/workspace/work/SelfDoc"
+export DL="$HOME/Downloads"
